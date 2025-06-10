ec99464075d9e979c48ed41a81b3f7c65bf2131f
Muzamil <muzamilzubair987654@gmail.com>
2025-06-10 12:51:02 +0500

    Update Files

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const inputBox = document.getElementById("input-box");
-const listContainer = documnet.getElementById("list-container");
+const listContainer = document.getElementById("list-container");
 
 function addTask(){
  if(inputBox. value === ''){
-    alert("You msut write something");
+    alert("You must write something");
  }
  else{
     let li = document.createElement("li");
     li.innerHTML = inputBox.value;
     listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7"
+    li.appendChild(span);
  }
+ inputBox.value = "";
+ saveData();
 }
+
+listContainer.addEventListener("click", function(e){
+   if(e.target.tagName === "LI")
+   {
+      e.target.classList.toggle("checked");
+   }
+   else if(e.target.tagName === "SPAN"){
+      e.target.parentElement.remove();
+   }
+}, false);
+
+function saveData(){
+   localStorage.setItem("data", listContainer.innerHTML);
+}
+function showTask(){
+   listContainer.innerHTML =localStorage.getItem("data")
+}
+showTask();
